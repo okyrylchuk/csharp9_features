5a191d894de1bf1d7177fb83c6ba8d2c5bb15a68
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 21:50:36 +0200

    Add ExtendedPartialMethods example

---
+++ ExtendedPartialMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExtendedPartialMethods
+{
+    // Example from tweet https://twitter.com/okyrylchuk/status/1416839495132065793
+    class Program
+    {
+        partial class Example
+        {
+            // Other than void return type is allowed
+            public partial int A();
+            // Access modifiers are allowed
+            public partial void B();
+            // Out params are allowed
+            public partial void C(out int param);
+        }
+
+        partial class Example
+        {
+            public partial int A() => 0;
+            public partial void B() { }
+            public partial void C(out int param)
+            {
+                param = 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

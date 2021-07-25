8a90111e938e854b4a7868c8898ee0a8ab43684a
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 21:09:22 +0200

    Add ImprovedTargetTyping examples

---
+++ ImprovedTargetTyping/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ImprovedTargetTyping
+{
+    // Example from tweet https://twitter.com/okyrylchuk/status/1412139149583437833
+    // And from tweet https://twitter.com/okyrylchuk/status/1412502863356469253
+    // To run C#8 version change <TargetFramework>netcoreapp3.1</TargetFramework> in csproj file
+    class Program
+    {
+        class Person { }
+
+        class Student : Person { }
+
+        class Customer : Person { }
+
+        static void Main(string[] args)
+        {
+            // Target-typed new expression
+            Student student = new ();
+            // Target-typed new expression
+            Customer customer = new ();
+            bool isStudent = true;
+
+            // Target-typed conditional ?:
+            Person person = isStudent ? student : customer;
+        }
+    }
+}

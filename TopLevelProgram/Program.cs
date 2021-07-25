8db48a28d8fa686fb0cb8f6b19689dee3de99e62
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 21:18:53 +0200

    Add TopLevelProgram example

---
+++ TopLevelProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// Example from tweet https://twitter.com/okyrylchuk/status/1413589024464424963
+
+Console.WriteLine(args);
+await Task.Delay(1000);
+return 0;
+
+#region C#8 version
+// To run C#8 version uncomment code below, comment C#9 version
+// And change <TargetFramework>netcoreapp3.1</TargetFramework> in csproj file
+/*class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        Console.WriteLine(args);
+        await Task.Delay(1000);
+        return 0;
+    }
+}*/
+#endregion

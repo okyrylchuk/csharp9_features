8c3893240488f0f9fa8636aae2a53dacd87a68dd
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 21:41:08 +0200

    Add LambdaDiscardParameters example

---
+++ LambdaDiscardParameters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LambdaDiscardParameters
+{
+    // Example from tweet https://twitter.com/okyrylchuk/status/1415747088592297986
+    class Program
+    {
+        class Button
+        {
+            public event EventHandler Click;
+        }
+
+        static void Main()
+        {
+            void PrintHelloWorld()
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            Button button = new();
+            button.Click += (s, e) => PrintHelloWorld();
+        }
+
+        #region C#8 version
+        // To run C#8 version uncomment code below, comment C#9 version
+        // And change <TargetFramework>netcoreapp3.1</TargetFramework> in csproj file
+        /*static void Main()
+        {
+            void PrintHelloWorld()
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+
+            var button = new Button();
+            button.Click += (s, e) => PrintHelloWorld();
+        }*/
+        #endregion
+    }
+}

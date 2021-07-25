a124e6ab3d127afb8bf10d3765db7f5400243cb1
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 22:03:45 +0200

    Add Records example

---
+++ Records/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Records
+{
+    // Example from tweet https://twitter.com/okyrylchuk/status/1418310978358423552
+    class Program
+    {
+        record Person
+        {
+            // No constructor required to initialize properties
+            public string FirstName { get; init; } // immutable
+            public string LastName { get; init; } // immutable
+        }
+
+        static void Main()
+        {
+            var person = new Person
+            {
+                FirstName = "Oleg",
+                LastName = "Kyrylchuk"
+            };
+
+            // Use with-expression to create new record from existing
+            // specifying the changes in the values of properties
+            var bondPerson = person with { LastName = "Bond" };
+
+            // Value base comparison
+            var duplicatedPerson = new Person
+            {
+                FirstName = "Oleg",
+                LastName = "Kyrylchuk"
+            };
+            person.Equals(duplicatedPerson); // true
+            _ = person == duplicatedPerson;  // true
+        }
+
+        #region C#8 version
+        // To run C#8 version uncomment code below, comment C#9 version
+        // And change <TargetFramework>netcoreapp3.1</TargetFramework> in csproj file
+        /*class Person
+        {
+            // The constructor is required to initialize properties
+            public Person(string firstName, string lastName)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+            public string FirstName { get; } // immutable
+            public string LastName { get; }  // immutable
+        }
+
+        static void Main()
+        {
+            var person = new Person("Oleg", "Kyrylchuk");
+
+            // No with-expression. The values to be copied
+            // should be explicitly passed into the constructor
+            var bondPerson = new Person(person.FirstName, "Bond");
+
+            // Reference base comparison
+            var duplicatedPerson = new Person("Oleg", "Kyrylchuk");
+            person.Equals(duplicatedPerson); // false
+            _ = person == duplicatedPerson;  // false
+        }*/
+        #endregion
+    }
+}

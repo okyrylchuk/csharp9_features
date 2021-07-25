54147e56234d0e99b70619d188bc68a0fa83cb5e
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 21:59:45 +0200

    Add FunctionPointers example

---
+++ FunctionPointers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FunctionPointers
+{
+    // Example from tweet https://twitter.com/okyrylchuk/status/1417892125912489985
+    class Program
+    {
+        unsafe class Example
+        {
+            // This method has a managed calling convention.
+            // This is the same as leaving the 'managed' keyword off.
+            delegate*<int, void> functionPointer1;
+
+            // The same as functionPointer1, but with explicit 'managed' keyword
+            delegate* managed<int, void> functionPointer2;
+
+            // This method will be invoked using whatever the default unmanaged calling
+            // convention on the runtime platform is. This is platform and architecture
+            // dependent and is determined by the CLR at runtime.
+            public delegate* unmanaged<int, void> functionPointer3;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

936bae268469b5fc76640bb26583fbf2f4d6686b
okyrylchuk <kyrylchuk.oleg@gmail.com>
2021-07-25 21:39:11 +0200

    Add AttributesOnLocalFunctions

---
+++ AttributesOnLocalFunctions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AttributesOnLocalFunctions
+{
+    // Example from tweet https://twitter.com/okyrylchuk/status/1415385617018490881
+    class Program
+    {
+        static void Main()
+        {
+            [Obsolete("Attribute on local function")]
+            void LocalFunction()
+            {
+                Console.WriteLine("Hello World!");
+            }
+
+            LocalFunction();
+        }
+    }
+}
